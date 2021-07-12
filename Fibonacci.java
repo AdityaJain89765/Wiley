1e6aded803ed0a46c7a12be1e254d73bb227bc1a
Aditya Jain <68730634+AdityaJain89765@users.noreply.github.com>
2021-07-12 15:16:43 +0530

    Add files via upload
    Fibonacci using threads

---
+++ Fibonacci.java
@@ -0,0 +1,44 @@
+package thread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fibonacci{
+	List<String> ls = new ArrayList<String>();
+	int sum = 0;
+
+	public static void main(String[] args) throws InterruptedException {
+		Fibonacci ob = new Fibonacci();
+		DoYourThing ob1 = new DoYourThing();
+		ob1.start();
+		for(int x=1;x<=10;x++) {
+			String st=ob.sum+" + "+ob1.b+" = "+(ob1.b+ob.sum);
+			ob.ls.add(st);
+			ob.sum+=ob1.b;
+			Thread.sleep(500);
+		}
+		Thread.sleep(1000);
+		System.out.println(ob.ls);
+		
+	}
+}
+
+class DoYourThing extends Thread {
+	int a = 0, b = 1;
+
+	public void run() {
+
+		for (int x = 1; x <= 10; x++) {
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			int c = a + b;
+			a = b;
+			b = c;
+		}
+	}
+}
